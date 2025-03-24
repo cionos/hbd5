c2ce7160781d97a302c4ddec13733cef84846389
cionos <huj90062@gmail.com>
2025-03-25 06:23:13 +0800

    Add files via upload

---
+++ particles.js
@@ -0,0 +1,12 @@
+<div id="particles-js"></div>
+<script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
+<script>
+    particlesJS("particles-js", {
+        "particles": {
+            "number": { "value": 100 },
+            "size": { "value": 3 },
+            "move": { "speed": 2 },
+            "line_linked": { "enable": true, "opacity": 0.5 }
+        }
+    });
+</script>
